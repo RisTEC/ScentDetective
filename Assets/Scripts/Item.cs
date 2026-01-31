8f2d61658120d0905c0ca78f9b8f21dd5f0a66fb
PurpleHatsOnCats <seth.vandivere@gmail.com>
2026-01-31 13:41:22 -0500

    Added item manager

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Set this to true when it is not located above a tile; manually set the below information in inspector
+    public bool ManualLocation;
+    public Vector2Int gridPos;
+    public float level;
+
+}
---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    private List<Item> items;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Call this method to update any new items added to scene. 
+    /// Automatically assigns a gridPos and layer, which can manually be changed later.
+    /// </summary>
+    [ContextMenu("Update Item Location")]
+    public void AssignTiles()
+    {
+        // Get item script from children
+        items = new List<Item>(this.GetComponentsInChildren<Item>());
+        
+        // Raycast down from each item to find a matching gridTile
+        int unassignedItems = 0;
+        foreach(Item item in items)
+        {
+            if(item.ManualLocation){continue;}
+
+            RaycastHit hitInfo;
+            Debug.DrawRay(item.transform.position, Vector3.down, Color.blue, 10.0f);
+            if(Physics.Raycast(item.transform.position, Vector3.down, out hitInfo , Mathf.Infinity))
+            {
+                Debug.Log(hitInfo);
+                // Copy gridTile info over
+                GridTile hitTile= hitInfo.collider.GetComponent<GridTile>();
+                item.gridPos = hitTile.gridPos;
+                item.level = hitTile.level;
+            }
+            else
+            {
+                item.gridPos = Vector2Int.zero;
+                item.level = 0;
+                unassignedItems++;
+            }
+        }
+        Debug.Log("Items that need assigning: " + unassignedItems);
+    }
+}
