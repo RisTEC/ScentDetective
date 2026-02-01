874016589ab851b08d4aac1c18dc23c94c041c68
PurpleHatsOnCats <seth.vandivere@gmail.com>
2026-02-01 15:09:16 -0500

    Added UI hint

--- Assets/Scripts/ScentManager.cs
+++ Assets/Scripts/ScentManager.cs
@@ -15,18 +15,28 @@ public class ScentManager : MonoBehaviour
     public int CurrentScent = 0;
     public OdorAsset SelectedScent;
     public GameObject TrailPrefab;
-    public Dictionary<string,string> characterSmells = new Dictionary<string, string>{
+    public Dictionary<string, string> characterSmells = new Dictionary<string, string>{
         {"Savory Spice","Chef"},
         {"Terra Silva","Gardener"},
         {"Timber","Lumberjack"},
         {"Machina","Mechanic"},
         {"Floral","Millionaire"},
         {"Kindred","Nanny"}};
+    public Dictionary<string, Color> scentColors;
     private float smellRange = 20.0f;
     public SelectedScentUI scentUI;
 
     void Start()
     {
+        scentColors = new Dictionary<string, Color>
+            {
+                {"Savory Spice", StringToColor("#A8632C")},
+                {"Terra Silva", StringToColor("#677166")},
+                {"Timber", StringToColor("#815D4D")},
+                {"Machina", StringToColor("#333333")},
+                {"Floral", StringToColor("#E87D9B")},
+                {"Kindred", StringToColor("#B399C9")}
+            };
         Instance = this;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
         SelectedScent = listOfScents[CurrentScent];
@@ -48,13 +58,22 @@ public class ScentManager : MonoBehaviour
             Sniff();
         }
     }
+    private Color StringToColor(string text)
+    {
+        if(UnityEngine.ColorUtility.TryParseHtmlString(text, out Color color))
+        {
+            return color;
+        }
+        Debug.LogWarning("Could not interpret color");
+        return Color.white;
+    }
     public void IncrementSelectedScent(int amount)
     {
         CurrentScent = (CurrentScent + amount + listOfScents.Count) % listOfScents.Count;
-            SelectedScent = listOfScents[CurrentScent];
-            UpdateScentUI();
+        SelectedScent = listOfScents[CurrentScent];
+        UpdateScentUI();
     }
-    
+
     void UpdateScentUI()
     {
         if (scentUI != null)
@@ -62,7 +81,7 @@ public class ScentManager : MonoBehaviour
             scentUI.SelectScent(SelectedScent.name);
         }
     }
-    
+
     public void Sniff()
     {
         int itemIndex = -1;
@@ -70,8 +89,8 @@ public class ScentManager : MonoBehaviour
         List<Vector2Int> itemPath = null;
 
         for (int i = 0; i < ItemManager.Instance.Items.Count; i++)
-        {   
-            if(ItemManager.Instance.Items[i].discovered)
+        {
+            if (ItemManager.Instance.Items[i].discovered)
                 break;
             if (ItemManager.Instance.Items[i].scent == SelectedScent)
             {
@@ -91,7 +110,7 @@ public class ScentManager : MonoBehaviour
         }
         if (itemPath != null && itemPath.Count <= smellRange)
         {
-            CreateTrail(itemPath, itemIndex, Mathf.Pow(1 - minDistance / smellRange,2)*2.0f);
+            CreateTrail(itemPath, itemIndex, Mathf.Pow(1 - minDistance / smellRange, 2) * 2.0f);
             OlfactoryEpithelium.Get().PlayOdor(SelectedScent, Mathf.Ceil((1 - minDistance / smellRange) * 4.0f) * (255.0f / 4.0f));
         }
     }
@@ -104,7 +123,7 @@ public class ScentManager : MonoBehaviour
             Trail trail = Instantiate(TrailPrefab, Vector3.zero, Quaternion.identity).GetComponent<Trail>();
             trail.path = itemPath;
             trail.currentIndex = 1;
-            trail.trailLength = 4+2;
+            trail.trailLength = 4 + 2;
             trail.intensity = intensity;
             trail.SetForTile(trail.currentIndex);
             trail.UpdateIntensity(false);
--- Assets/Scripts/SelectedScentUI.cs
+++ Assets/Scripts/SelectedScentUI.cs
@@ -2,10 +2,12 @@ using System;
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using UnityEngine.UI;
 public class SelectedScentUI : MonoBehaviour
 {
     public TextMeshProUGUI scentNameText;
     private List<GameObject> models;
+    public Button sniffButton;
     public void Awake()
     {
         models = new List<GameObject>();
@@ -19,22 +21,31 @@ public class SelectedScentUI : MonoBehaviour
     }
     public void SelectScent(String scent)
     {
-        Debug.Log("Scent selected: " + scent);
+        // Set name of character
         string displayText = "";
         if (ScentManager.Instance.characterSmells.ContainsKey(scent))
         {
             displayText += ScentManager.Instance.characterSmells[scent] + ": ";
         }
         scentNameText.text =  displayText + scent;
+
+        // Display correct model
         foreach(GameObject model in models)
         {
-            Debug.Log("Testing: " + model.name);
-            if(model.name == ScentManager.Instance.characterSmells[scent])
-            {
-                Debug.Log("Showing: " + model.name);
-            }
             model.GetComponent<MeshRenderer>().enabled = 
                 model.name == ScentManager.Instance.characterSmells[scent];
+        }
+
+        // Change color of button
+        sniffButton.image.color = ScentManager.Instance.scentColors[scent];
+        if(sniffButton.image.color.grayscale < 0.3f)
+        {
+            // Make text white if the color is too dark
+            sniffButton.GetComponentInChildren<TextMeshProUGUI>().color = new Color(0.75f, 0.75f, 0.75f);
+        }
+        else
+        {
+            sniffButton.GetComponentInChildren<TextMeshProUGUI>().color = Color.black;
         }
     }
 }
